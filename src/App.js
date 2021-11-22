b70016778323274b5f41723e99f5ac792852ed10
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2021-11-22 17:58:08 +0200

    Added axios get method

--- src/App.js
+++ src/App.js
@@ -1,17 +1,56 @@
 import React from 'react';
+import axios from "axios";
 import Header from "./components/Header";
 import Footer from "./components/Footer"
 
 
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+
+  state ={
+    data:[],
+  };
+
+  componentDidMount(){
+    axios.get('https://restcountries.com/v3.1/all').then((res) => {this.setState({data: res.data})
+    console.log(this.state.data);
+  });
+  };
+
+ 
+      
+ 
+  
+
+  
+  render() {
+
+    
+    return (
+    <div>
       <Header/>
-      <h1>Hello world</h1>
+
+       <div className="countries">
+      {
+        this.state.data.map
+        (country=>
+          <div className="country" key = {country.name.common} >
+            <img src={country.flags.png} alt="country flag"/>
+            <h2>{country.name.common} </h2>
+            <p>{country.capital}</p>
+            <p>Population: {country.population.toLocaleString()}</p>
+            <p>Borders with: <span></span></p>
+          </div>
+        )
+      }
+      </div> 
       <Footer/>
+  
     </div>
-  );
+    );
+  }
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./styles/App.css";
+import "./styles/card.css";
 import App from './App';
 
 
